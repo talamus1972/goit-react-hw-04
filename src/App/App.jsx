85cadbbda17cbac7060ff35028ca9bd420c72009
talamus1972 <talamus1972@gmail.com>
2024-03-02 12:54:55 +0200

    11

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -6,68 +6,68 @@ import ErrorMessage from "../ErrorMessage/ErrorMessage.jsx";
 import Loader from "../Loader/Loader.jsx";
 import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn.jsx";
 
-
-
 export default function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [page, setPage] = useState(1);
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
-  
+
   // 572515
   // Authorization: Client-ID YOUR_ACCESS_KEY
   //  gZuYWweutzjOLPBSOxTd_JFkbovfFGxduFYh-JTDS9w
   // https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
 
-axios.defaults.baseURL="http://hn.algolia.com/api/v1"
+  // axios.defaults.baseURL="http://hn.algolia.com/api/v1"
+  axios.defaults.baseURL =
+    "https://api.unsplash.com/photos/?client_id=gZuYWweutzjOLPBSOxTd_JFkbovfFGxduFYh-JTDS9w";
 
   useEffect(() => {
     if (searchQuery === "") {
-      return
+      return;
     }
     async function getData() {
       try {
-      setIsLoading(true);
-      setError(false)
-      const response = await axios.get("/search", {
-        params: {
-          query: searchQuery,
-          hitsPerPage: 10,
-          page
-        }
-      });
-        const data= response.data.hits
+        setIsLoading(true);
+        setError(false);
+        const response = await axios.get("/search", {
+          params: {
+            query: searchQuery,
+            hitsPerPage: 10,
+            page,
+          },
+        });
+        const data = response.data.results;
         setArticles((prevAticles) => {
-        return [...prevAticles, ...data]
-      });
-    } catch (e) {
-      setError(true)
-    } finally {
-      setIsLoading(false);
-      
+          return [...prevAticles, ...data];
+        });
+      } catch (e) {
+        setError(true);
+      } finally {
+        setIsLoading(false);
+      }
     }
-    }
-    getData()
-  }, [page, searchQuery]); 
-  
-  
+    getData();
+  }, [page, searchQuery]);
+
   const handleSearch = async (newQuery) => {
     setSearchQuery(newQuery);
-    setPage(1)
-    setArticles([])
+    setPage(1);
+    setArticles([]);
   };
 
   const handleLoadMore = () => {
-    setPage(page + 1)
-  }
+    setPage(page + 1);
+  };
   return (
     <div>
       <SearchBar onSearch={handleSearch} />
       {isLoading && <Loader loading={isLoading} />}
       {error && <ErrorMessage />}
       {articles.length > 0 && <ImageGallery items={articles} />}
-      {articles.length > 0 && !isLoading && <LoadMoreBtn onClick={handleLoadMore} />}
+      {articles.length > 0 && !isLoading && (
+        <LoadMoreBtn onClick={handleLoadMore} />
+      )}
     </div>
   );
 }
